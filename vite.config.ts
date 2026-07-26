b99e572b73759eb819a4ec24f6dc15ae89abf221
Luciano Veras <lucianoveras.dev@gmail.com>
2026-07-26 16:28:32 -0300

    Configure GitHub Pages deployment and title

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/erp-compras/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
